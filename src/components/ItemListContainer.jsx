91340f2744ef826d8fdefceea2d7b3e3de01e23e
Jose Ignacio Thames <joseltrcthames78@gmail.com>
2024-03-30 16:33:41 -0300

    ac1

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
+
 import Container from 'react-bootstrap/Container';
 
-export const ItemListContainer = ({greeting}) =>(
-<Container className="mt-4">
-     <h1>{greeting}</h1>
-     </Container>
-); 
+import data from "../data/products.json";
+
+export const ItemListContainer = () =>{
+     const [products, setProducts] = useState([])
+
+     useEffect(() => {
+      const get = new Promise((resolve, reject) => {
+          setTimeout(() => resolve(data), 2000);
+      });
+      
+      get.them((data) => {
+          setProducts(data);
+     });
+     }, []);
+
+return (
+ <Container className="mt-4">{products.map((product) => (  
+<h1>{product.name}</h1>
+))}
+</Container>
+);
+};
+
